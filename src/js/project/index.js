c75327754e6404402431b7a52ecb1f108aa1fdee
Anna-Anka <anjbekrenewa@gmail.com>
2024-12-10 18:29:09 +0300

    add gallery-page

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -1,3 +1,4 @@
 import './_menu-item-highlight.js';
 import './_header-search-mobile.js';
 import './sliders/_partners-slider.js';
+import './sliders/_gallery-slider.js';
---
+++ src/js/project/sliders/_gallery-slider.js
@@ -0,0 +1,45 @@
+import Swiper from 'swiper';
+import { Navigation, Thumbs } from "swiper/modules";
+
+Swiper.use([Navigation, Thumbs]);
+
+const sliderMain = document.querySelector('.gallery-page .slider-main');
+const sliderNav = document.querySelector('.gallery-page .slider-nav');
+const wrapperSliderNav = document.querySelector('.gallery-page__thumbs')
+
+if (sliderNav && sliderMain && wrapperSliderNav) {
+    const swiperSmall = new Swiper(sliderNav, {
+        
+        loopedSlides: 4,
+        freeMode: true,
+
+        breakpoints: {
+            769: {
+                direction: 'vertical',
+                slidesPerView: 4,
+                spaceBetween: 16,
+                slidesPerGroup: 4,
+            },
+
+            0: {
+                direction: 'horizontal',
+                slidesPerView: 'auto',
+                spaceBetween: 8,
+            }
+        }
+    });
+
+    new Swiper(sliderMain, {
+        spaceBetween: 10,
+        loopedSlides: 4,
+
+        thumbs: {
+            swiper: swiperSmall,
+        },
+
+        navigation: {
+            nextEl: sliderMain.querySelector('.gallery-page__button--prev'),
+            prevEl: sliderMain.querySelector('.gallery-page__button--next'),
+        },
+    });
+}
