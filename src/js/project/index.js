acbca89654c2d246d1e80ea35410ccbf7f839119
Anna-Anka <anjbekrenewa@gmail.com>
2024-12-17 12:10:14 +0300

    start working with map

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -1,6 +1,7 @@
 import './_menu-item-highlight.js';
 import './_header-search-mobile.js';
 import './_breadcrumbs-prev.js';
+import './map/_map.js';
 import './sliders/_partners-slider.js';
 import './sliders/_gallery-slider.js';
 import './sliders/_office-slider.js';
