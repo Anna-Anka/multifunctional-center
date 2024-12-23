0fc94f56bf27e425a52ac828da5324bf61f14dd0
Anna-Anka <anjbekrenewa@gmail.com>
2024-12-23 18:01:48 +0300

    widget

--- src/js/project/_vk.js
+++ src/js/project/_vk.js
@@ -1,22 +1,10 @@
-import * as VKID from '@vkid/sdk';
+import {Config} from '@vkid/sdk';
 
-VKID.Config.init({
+Config.init({
     app: 52881883,
     redirectUrl: 'https://mysite.com',
-    state: 'state',
-    codeVerifier: 'codeVerifier',
-    scope: 'phone email',
 });
 
-
-const authButton = document.createElement('button');
-authButton.onclick = () => {
-    // После авторизации будет редирект на адрес, указанный в параметре redirectUrl
-    VKID.Auth.login()
-        .catch(console.error);
-};
-
-document.getElementById('container').appendChild(authButton);
 
 // VK.Widgets.CommunityMessages("vk_community_messages", 433458018);
 // VK.Widgets.CommunityMessages("vk_community_messages", 52881883);
--- src/js/project/index.js
+++ src/js/project/index.js
@@ -3,7 +3,9 @@ import './_header-search-mobile.js';
 import './_breadcrumbs-prev.js';
 import './_services-filters.js';
 import './map/_map.js';
-import './_vk.js';
+// import './_vk.js';
 import './sliders/_partners-slider.js';
 import './sliders/_gallery-slider.js';
 import './sliders/_office-slider.js';
+
+VK.Widgets.CommunityMessages("vk_community_messages", 52881883);
