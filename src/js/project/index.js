6ff58ca7bb67ba7beffb5667b36c0622a51f15e8
Anna-Anka <anjbekrenewa@gmail.com>
2024-12-23 19:31:25 +0300

    remove widget, fix breadcrumbs, update services img

--- src/js/project/index.js
+++ src/js/project/index.js
@@ -3,7 +3,6 @@ import './_header-search-mobile.js';
 import './_breadcrumbs-prev.js';
 import './_services-filters.js';
 import './map/_map.js';
-import './_vk.js';
 import './sliders/_partners-slider.js';
 import './sliders/_gallery-slider.js';
 import './sliders/_office-slider.js';
