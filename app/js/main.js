ab2e15555a733b1098ca1e6f6de0512688ae6dc0
Anna-Anka <anjbekrenewa@gmail.com>
2024-12-27 17:33:39 +0300

    add favicon

--- app/js/main.js
+++ app/js/main.js
@@ -487,7 +487,6 @@ _project_index_js__WEBPACK_IMPORTED_MODULE_3__ = (__webpack_async_dependencies__
 
 
 
-console.log('gulp');
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } });
 
