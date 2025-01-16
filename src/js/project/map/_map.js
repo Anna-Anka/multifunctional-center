7fc02e05e0aeecfe4ceddfec260c69b6b3af1c13
Anna-Anka <anjbekrenewa@gmail.com>
2025-01-16 12:29:51 +0300

    test json

--- src/js/project/map/_map.js
+++ src/js/project/map/_map.js
@@ -58,11 +58,12 @@ function createPin(pin) {
 }
 
 (function () {
-    fetch('../../../assets/advance-pins.json')
+    fetch('../assets/advance-pins.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
             }
+            console.log(response.json())
             return response.json();
         })
         .then(data => {
